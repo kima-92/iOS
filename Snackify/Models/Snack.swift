8097aae39ea88d84480e20abc2bc6bc2f1e5c053
Jon Bash <jonbash@protonmail.com>
2019-11-18 11:00:15 -0800

    add snack model

--- Snackify/Models/Snack.swift
+++ Snackify/Models/Snack.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+// MARK: - Snack
+
+struct Snack {
+    let name: String
+    let type: String
+    let numberOfServings: Int
+    let nutritionInfo: NutritionInfo
+    let totalWeight: Double
+    let price: Double
+    let imageData: Data
+}
+
+// MARK: - Nutrition Info
+
+struct NutritionInfo {
+    let calories: Double
+    let totalFat: Double
+    let totalSugars: Double
+    let protein: Double
+    let carbs: Double
+    let allergens: [Allergen]
+}
+
+// MARK: - Allergen
+
+enum Allergen: String {
+    case peanuts
+    case dairy
+    case egg
+    case gluten
+    case meat
+    case shellfish
+}
