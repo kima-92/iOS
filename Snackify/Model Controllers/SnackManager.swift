128cd8e8965b75c9553d8c967a9203d85684bb3d
Jon Bash <jonbash@protonmail.com>
2019-11-21 17:39:18 -0800

    remove unused code

--- Snackify/Model Controllers/SnackManager.swift
+++ Snackify/Model Controllers/SnackManager.swift
@@ -9,15 +9,18 @@
 import Foundation
 
 class SnackManager {
-    static let shared = SnackManager(networkManager: NetworkManager.shared)
     
-    var networkManager: NetworkManager = NetworkManager.shared
+    // MARK: - Properties
+    
+    var networkManager: NetworkManager
     
     let baseURL: URL
     
     var allSnacksOptions: [Snack]?
     var currentOrderSnacks: [Snack] = []
     var subsOrderDeadline: String = "11/23/2019"
+    
+    // MARK: - Init
     
     init(networkManager: NetworkManager) {
         self.networkManager = networkManager
@@ -33,6 +36,8 @@ class SnackManager {
             }
         }
     }
+    
+    // MARK: - Methods
     
     func fetchSnackOptions(completion: @escaping (Result<[Snack],NetworkError>) -> Void) {
         let request = networkManager.newRequest(
@@ -82,108 +87,4 @@ class SnackManager {
     func AddSnackToCurrentSubscription(snack: Snack) {
         currentOrderSnacks.append(snack)
     }
-    
-    func deleteSnackFromCurrentSubscription(snack: Snack) {
-        
-        //test
-        var currentListTest = currentOrderSnacks.map( { $0.name })
-        print("Current list \(currentListTest) ")
-        
-        var snack = currentOrderSnacks.filter( { $0.id != snack.id })
-        
-        //test
-        currentListTest = currentOrderSnacks.map( { $0.name })
-        print("Current list after filtering \(currentListTest)")
-//        currentOrderSnacks.remove
-    }
-    
-    /// For regular/non-admin employees, make one-time purchases or request additions to the organization snack subscription. If user is an authorized organization administrator, purchase snacks as one-time orders or add them to their regular subscription.
-//    func handleOneTimeSnackPurchase(snacks: [Snack], completion: @escaping (Result<Bool,NetworkError>) -> Void) {
-//        guard let userData = networkManager.user?.toJSONData() else {
-//            completion(.failure(.noEncode))
-//            return
-//        }
-//
-//        #warning("This URL is currently invalid. Modify with actual URL component(s) before using.")
-//        let request = networkManager.newRequest(
-//            url: baseURL.appendingPathComponent("INSERT PATH COMPONENT(s) HERE"),
-//            method: .post,
-//            body: userData)
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            if !self.networkManager.dataTaskDidSucceed(with: response) {
-//                completion(.failure(.otherError))
-//                return
-//            }
-//
-//            if let error = error {
-//                print(error)
-//                completion(.failure(.otherError))
-//                return
-//            }
-//            // TODO: Add any further handling of response, data
-//            completion(.success(true))
-//        }.resume()
-//    }
-    
-//    func fetchSubscription(completion: @escaping (Result<Subscription,NetworkError>) -> Void) {
-//        #warning("This URL is currently invalid. Modify with actual URL component(s) before using.")
-//        let request = networkManager.newRequest(
-//            url: baseURL.appendingPathComponent("TEMP"),
-//            method: .get)
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            if !self.networkManager.dataTaskDidSucceed(with: response) {
-//                completion(.failure(.otherError))
-//                return
-//            }
-//
-//            if let error = error {
-//                print(error)
-//                completion(.failure(.otherError))
-//                return
-//            }
-//
-//            guard let data = data else {
-//                completion(.failure(.badData))
-//                return
-//            }
-//
-//            do {
-//                let subscription = try JSONDecoder().decode(Subscription.self, from: data)
-//                completion(.success(subscription))
-//            } catch {
-//                print(error)
-//                completion(.failure(.noDecode))
-//            }
-//        }.resume()
-//    }
-    
-//    func updateSubscription(_ subscription: Subscription, completion: @escaping (Result<Bool,NetworkError>) -> Void) {
-//        guard let userData = networkManager.user?.toJSONData() else {
-//            completion(.failure(.noEncode))
-//            return
-//        }
-//
-//        #warning("This URL is currently invalid. Modify with actual URL component(s) before using.")
-//        let request = networkManager.newRequest(
-//            url: baseURL.appendingPathComponent("INSERT PATH COMPONENT(s) HERE"),
-//            method: .post,
-//            body: userData)
-//
-//        URLSession.shared.dataTask(with: request) { data, response, error in
-//            if !self.networkManager.dataTaskDidSucceed(with: response) {
-//                completion(.failure(.otherError))
-//                return
-//            }
-//
-//            if let error = error {
-//                print(error)
-//                completion(.failure(.otherError))
-//                return
-//            }
-//            // TODO: Add any further handling of response, data
-//            completion(.success(true))
-//        }.resume()
-//    }
 }
