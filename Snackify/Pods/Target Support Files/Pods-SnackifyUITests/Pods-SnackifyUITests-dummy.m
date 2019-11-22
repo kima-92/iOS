52b6417dae250543e47b3da8eb5f9df726e74194
Wilma <kima_92@fastmail.com>
2019-11-22 10:41:14 -0500

    Installed Cocoa Pods. Added SwiftLint is Pod file, but it's not recognizable

---
+++ Snackify/Pods/Target Support Files/Pods-SnackifyUITests/Pods-SnackifyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnackifyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SnackifyUITests
+@end
--- Snackify/View Controllers/LoginViewController.swift
+++ Snackify/View Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import swiftLi
 
 class LoginViewController: UIViewController {
     
